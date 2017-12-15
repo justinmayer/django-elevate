b74ca5caca6aa78f040875adb43d64d4e409fa24
Justin Mayer <entroP@gmail.com>
2017-12-15 14:18:07 -0800

    Auto-focus input on password field

--- docs/changelog/index.rst
+++ docs/changelog/index.rst
@@ -4,4 +4,5 @@ Changelog
 2.2.0a1
 ~~~~~~~
 * Add support for Django 1.11 and 1.10
+* Auto-focus input on password field
 * Fork and rename project
--- elevate/forms.py
+++ elevate/forms.py
@@ -14,7 +14,9 @@ from django.utils.translation import ugettext_lazy as _
 class ElevateForm(forms.Form):
     """
     A simple password input form used by the default :func:`~elevate.views.elevate` view. """
-    password = forms.CharField(label=_('Password'), widget=forms.PasswordInput)
+    password = forms.CharField(
+        label=_('Password'), widget=forms.PasswordInput(attrs={'autofocus': True})
+    )
 
     def __init__(self, user, *args, **kwargs):
         self.user = user
