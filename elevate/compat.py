1cafc7830afeb668e84812ba144fc638a296bd95
gabn88 <gerben_m88@hotmail.com>
2019-01-29 16:47:44 +0100

    Update compat.py

--- elevate/compat.py
+++ elevate/compat.py
@@ -2,4 +2,4 @@ from django.utils.http import is_safe_url
 
 def is_safe_url(url, allowed_hosts):
     # This is the compat function for older Django versions
-    return is_safe_url(url, host=allowed_hosts)
+    return is_safe_url(url=url, host=allowed_hosts)
